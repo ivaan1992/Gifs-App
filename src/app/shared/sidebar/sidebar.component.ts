5d49502d3b6a661087535c1291679e39bf072dac
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-05-20 20:14:10 -0500

    Add components, service, modules, and sytart a search app

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+
+  get searchHistory() {
+    return this.gifsService.history;
+  }
+
+  constructor( private gifsService: GifsService ) { }
+
+
+}
